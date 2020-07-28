72577a7418077687a805786748d23290896073ea
Biljana <zabiljanupavlovic@gmail.com>
2020-07-29 00:34:38 +0200

    page built

---
+++ docs/precache-manifest.152d45df7e7fac5f025d80010355cded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c763778fa08590e430208cb51405a16b",
+    "url": "/Tic-tac-toe-game/index.html"
+  },
+  {
+    "revision": "69f43d65d45f2858c5b5",
+    "url": "/Tic-tac-toe-game/static/css/main.19a8f041.chunk.css"
+  },
+  {
+    "revision": "fc90276544ca7889101d",
+    "url": "/Tic-tac-toe-game/static/js/2.f0674c3d.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/Tic-tac-toe-game/static/js/2.f0674c3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69f43d65d45f2858c5b5",
+    "url": "/Tic-tac-toe-game/static/js/main.648ea964.chunk.js"
+  },
+  {
+    "revision": "2d73f417be0e0de96dc2",
+    "url": "/Tic-tac-toe-game/static/js/runtime-main.dfa13d5f.js"
+  }
+]);
